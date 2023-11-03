f917b5f85998c0efed392eb7e3154219cc8b31bd
Rosny Fletez Jr. <rfletezjr@gmail.com>
2023-11-03 14:54:03 -0700

    Created a Spring Boot app for the backend

---
+++ backend/UserLogin/src/main/java/com/login/UserLogin/UserLoginApplication.java
@@ -0,0 +1,13 @@
+package com.login.UserLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserLoginApplication.class, args);
+	}
+
+}
