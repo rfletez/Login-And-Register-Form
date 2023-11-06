8d8ecc5bd1c7aef2cf1d63c3c602dbfc5f482f7d
Rosny Fletez Jr. <rfletezjr@gmail.com>
2023-11-06 15:03:45 -0700

    Created three components on frontend

---
+++ frontend/src/components/Register.jsx
@@ -0,0 +1,55 @@
+function Register() {
+
+    
+
+    return (
+        <div>
+            <div class="container mt-4">
+                <div class="card">
+                    <h1>Employee Registration</h1>
+
+                    <form>
+                        <div class="form group">
+                            <label>Employee Name</label>
+                            <input type="text" class="form-control" 
+                                id="employee_name" 
+                                placeholder="Enter your name..."
+                                value={employeeName}
+                                onChange={(event) => {
+                                    setEmployeeName(event.target.value);
+                                }} 
+                            />
+                        </div>
+
+                        <div class="form group">
+                            <label>Employee Email</label>
+                            <input type="text" class="form-control"
+                                id="employee_email" 
+                                placeholder="Enter your email..."
+                                value={email}
+                                onChange={(event) => {
+                                    setEmail(event.target.value);
+                                }} />
+                        </div>
+
+                        <div class="form group">
+                            <label>Employee Password</label>
+                            <input type="text" class="form-control"
+                                id="employee_password" 
+                                placeholder="Enter your password..."
+                                value={password}
+                                onChange={(event) => {
+                                    setPassword(event.target.value);
+                                }} />
+                        </div>
+
+                        <button type="submit" class="btn btn-primary mt-4" onClick={save}>Save</button>
+
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+export default Register;
