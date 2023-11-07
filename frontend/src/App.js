4f582b59e011fa6faeab48951d7b9a3b72e9a886
Rosny Fletez Jr. <rfletezjr@gmail.com>
2023-11-07 08:52:32 -0700

    Updated Login component and added routers on App file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './components/Login';
+import Home from './components/Home';
+import Register from './components/Register';
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Login />}>
+            <Route index element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,12 +1,45 @@
 import { useState } from 'react';
 import { axios } from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
 
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    async function login(event) {
+        event.preventDefault();
+
+        try {
+            await axios.post("http://localhost:8101/api/v1/com/employeePath/login", {
+                email: email,
+                password: password,
+            }).then((result) => {
+                console.log(result.data);
+
+                if(result.data.message === "Email does not exists") {
+                    alert("Email does not exists");
+                }
+                else if(result.data.message === "Login successful") {
+                    navigate("/home");
+                }
+                else {
+                    alert("Email and Password do not match");
+                }
+            }, fail => {
+                console.error(fail);
+            });
+        }
+        catch(err) {
+            alert(err);
+        }
+    }
 
     return(
         <div>
             <div className='container'>
+
                 <div className='row'>
                     <h2>Login</h2>
                     <hr/>
@@ -16,11 +49,34 @@ function Login() {
                     <div className='col-sm-6'>
                         <form>
                             <div className='form-group'>
-                                
+                                <label>Email</label>
+                                <input type='email' 
+                                    className='form-control' 
+                                    id='email' 
+                                    placeholder='Enter email...'
+                                    value={email}
+                                    onChange={(event) => {
+                                        setEmail(event.target.value)
+                                    }} />
                             </div>
+
+                            <div className='form-group'>
+                                <label>Password</label>
+                                <input type='password' 
+                                    className='form-control' 
+                                    id='password' 
+                                    placeholder='Enter password...'
+                                    value={password}
+                                    onChange={(event) => {
+                                        setPassword(event.target.value)
+                                    }} />
+                            </div>
+
+                            <button type='submit' className='btn btn-primary' onClick={login}>Login</button>
                         </form>
                     </div>
                 </div>
+
             </div>
 
         </div>
