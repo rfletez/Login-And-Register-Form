740d8719f530f9ace1dafd422b71d640a2e8a2e8
Rosny Fletez Jr. <rfletezjr@gmail.com>
2023-11-07 08:27:29 -0700

    Created Login Page

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import { axios } from 'axios';
+
+function Login() {
+
+
+    return(
+        <div>
+            <div className='container'>
+                <div className='row'>
+                    <h2>Login</h2>
+                    <hr/>
+                </div>
+
+                <div className='row'>
+                    <div className='col-sm-6'>
+                        <form>
+                            <div className='form-group'>
+                                
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+export default Login;
